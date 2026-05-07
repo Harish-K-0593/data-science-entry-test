8c6214909339c8ae5be22958238605f784169ffb
Harish <96907654+Harish-K-0593@users.noreply.github.com>
2026-05-07 17:32:52 +0800

    Rename Q1.py to q1.py

--- q1.py
+++ q1.py
@@ -1,18 +1,18 @@
 ''' Task 1:
-    # 1. Swap two values (only using x and y) with NO thrid helper variable
-    # 2. Both inputs must be numeric (integer or float). If either one is not, return "-1"
+    # 1. Swap two values (only using x and y) with NO third helper variable
+    # 2. Both inputs must be numeric (integer or float). If either one is not, return "-1".
     # 3. If both are numeric, print the swapped values
 '''
 
-# Defining the function swap with 2 parameters and setting the conditions that x & 7 must be either an integer or a float. To take note that Boolean Values are also read as integers in Python.
+# Defining the function swap with 2 parameters and setting the conditions that x & 7 must be either an integer or a float. To note that Boolean Values are also read as integers in Python.
 def swap(x,y):
     if isinstance (x, bool) or isinstance (y, bool) or not isinstance(x, (int, float)) or not isinstance (y, (int, float)): 
         return -1
     
-    # The swap. The right-hand sided y,x creates a tuple of the current values.
+    # The swap. The right-hand side of y,x creates a tuple of the current values.
     x, y = y, x
     
-    # Formatted string allows anything inside the curly braces to to get evaluated as Python code and inserted into the f string.
+    # Formatted string allows anything inside the curly braces to get evaluated as Python code and inserted into the f string.
     print(f"x = {x}, y = {y}")
     
 # Test: Calling the function must return x = 1000 & y = 10.    
